2be69e93c2a7fc276eeb93e40f00121502470e84
neofytr <imagination12344321.com@gmail.com>
2025-03-16 01:10:44 +0530

    fixed errors

--- bpe/inc/bpe.h
+++ bpe/inc/bpe.h
@@ -25,13 +25,8 @@ typedef struct
 bool is_less(const void *a, const void *b);
 char *get_file(const char *path);
 void print_text(const uint32_t *text, int length);
-void print_graph(dyn_arr_t *pair_arr, size_t len, const char *png_name, bool add_ascii);
-bool dump_pairs(const char *path, dyn_arr_t *pair_arr, uint32_t len);
+void print_graph(dyn_arr_t *pair_arr, const char *png_name, bool add_ascii);
+bool dump_pairs(const char *path, dyn_arr_t *pair_arr);
 dyn_arr_t *read_pairs(const char *path);
-
-dyn_arr_t *initialize_pair_array();
-uint32_t *initialize_text_buffer(const char *file_path, int *text_size);
-void free_resources(uint32_t *text, uint32_t *temp, char *text_buffer, dyn_arr_t *pair_arr);
-uint32_t *encode(uint32_t *text, int *text_size, dyn_arr_t *pair_arr);
 
 #endif
