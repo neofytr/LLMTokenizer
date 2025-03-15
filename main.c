8267d0ef97e7173c93fa361dcfc315f96679e8ca
neofytr <imagination12344321.com@gmail.com>
2025-03-15 18:18:18 +0530

    some changes

--- main.c
+++ main.c
@@ -12,11 +12,62 @@ typedef struct
     uint32_t a, b;
 } pair_t;
 
-int main()
+char *get_file(const char *path)
+{
+    FILE *file = fopen(path, "r");
+    if (!file)
+    {
+        perror("fopen");
+        return NULL;
+    }
+
+    if (fseek(file, 0, SEEK_END) == -1)
+    {
+        perror("fseek");
+        fclose(file);
+        return NULL;
+    }
+
+    long len = ftell(file);
+    if (len == -1)
+    {
+        perror("ftell");
+        fclose(file);
+        return NULL;
+    }
+
+    rewind(file);
+
+    char *arr = (char *)malloc(len + 1); // +1 for null terminator
+    if (!arr)
+    {
+        perror("malloc");
+        fclose(file);
+        return NULL;
+    }
+
+    size_t read_bytes = fread(arr, 1, len, file);
+    if (read_bytes < (size_t)len)
+    {
+        if (ferror(file))
+        {
+            perror("fread");
+            free(arr);
+            fclose(file);
+            return NULL;
+        }
+    }
+
+    arr[len] = '\0';
+
+    fclose(file);
+    return arr;
+}
+
+int main(int argc, char **argv)
 {
     int exit_code = EXIT_SUCCESS;
-    // taken from wikipedia article for BPE
-    const char *text = "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text.";
+    const char *text = get_file(argv[1]);
     int text_size = strlen(text);
 
     hash_table_t *table = hash_table_create(1024, sizeof(pair_t), sizeof(size_t));
@@ -66,6 +117,10 @@ error_handling:
     if (table)
     {
         hash_table_destroy(table);
+    }
+    if (text)
+    {
+        free(text);
     }
     return exit_code;
 }
